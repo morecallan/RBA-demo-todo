15c55e73349cb55bfc34c026746721144dc2eafd
Callan Morrison <callan.l.morrison@gmail.com>
2016-05-18 21:19:03 -0500

    bootstrap nav bar

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 
 var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", function($scope){
+    $scope.navItems = [{name: "All Items"}, {name: "New Item"}]
+})
+
+
 app.controller("TodoCtrl", function($scope){
     $scope.welcome = "hello";
 })

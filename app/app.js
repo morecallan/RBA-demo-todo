1a51aaa13c4085f388df1935ce7f67c7dbfe9993
Callan Morrison <callan.l.morrison@gmail.com>
2016-05-21 10:12:01 -0500

    view controllers for new item and list items

--- app/app.js
+++ app/app.js
@@ -8,4 +8,16 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
     $scope.welcome = "hello";
+    $scope.showListView = true;
+
+    $scope.newItem  = function(){
+        console.log("you clicked new item");
+        $scope.showListView = false;
+    };
+
+    $scope.allItems = function() {
+        console.log("you clicked all items");
+        $scope.showListView = true;
+    };
+
 });

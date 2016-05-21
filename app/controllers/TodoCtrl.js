0de40c049f25bc69259a0a0c9bcb0d6556a42755
Callan Morrison <callan.l.morrison@gmail.com>
2016-05-21 12:46:57 -0500

    broke out controllers

---
+++ app/controllers/TodoCtrl.js
@@ -0,0 +1,55 @@
+app.controller("TodoCtrl", function($scope){
+    $scope.welcome = "hello";
+    $scope.showListView = true;
+    $scope.newTask = {};
+
+
+    $scope.items = [
+        {
+            id: 0,
+            task: "mow the lawn",
+            isCompleted: true,
+            dueDate: "12/5/17",
+            assignedTo: "Greg",
+            location: "Zoe's house",
+            urgency: "low",
+            dependencies: "sunshine, clippers, hat, water"
+        },
+        {
+            id: 1,
+            task: "grade quizzes",
+            isCompleted: false,
+            dueDate: "12/5/15",
+            assignedTo: "Joe",
+            location: "NSS",
+            urgency: "high",
+            dependencies: "wifi, tissues, vodka"
+        },
+        {
+            id: 2,
+            task: "take a nap",
+            isCompleted: false,
+            dueDate: "5/21/17",
+            assignedTo: "zoe",
+            location: "Zoe's house",
+            urgency: "medium",
+            dependencies: "cat, pillow, blanket, hammock"
+        }
+    ];
+
+    $scope.newItem  = function(){
+        $scope.showListView = false;
+    };
+
+    $scope.allItems = function() {
+        $scope.showListView = true;
+    };
+
+    $scope.addNewItem = function() {
+        $scope.newTask.isCompleted = false;
+        $scope.newTask.id = $scope.items.length;
+        $scope.items.push($scope.newTask);
+        $scope.newTask = "";
+    };
+
+});

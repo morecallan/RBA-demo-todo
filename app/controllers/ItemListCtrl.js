86fbb3e79dcd6601dabfd8b60173cb7180dc2a87
Callan Morrison <callan.l.morrison@gmail.com>
2016-05-23 19:36:10 -0500

    delete button  and connection to firebase

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,12 +1,25 @@
 app.controller("ItemListCtrl", function($scope, $http){
     $scope.items = [];
 
-    $http.get("https://callan-todo.firebaseio.com/items/.json")
-    .success(function(itemObject){
-        var itemCollection = itemObject; //get to your object of objects
-        Object.keys(itemCollection).forEach(function(key){ //using the keys method on js's object. loops through the object and pulls out our keys and returns array of keys. for each of these keys
-            itemCollection[key].id=key; //go through the item collection object by each key and sets the id value to the key value
-            $scope.items.push(itemCollection[key]); //pushes each itemCollection object to the $scope array
-        });
-    });
+    var getItems = function(){
+        $http.get("https://callan-todo.firebaseio.com/items/.json")
+            .success(function(itemObject){
+                var itemCollection = itemObject; //get to your object of objects
+                Object.keys(itemCollection).forEach(function(key){ //using the keys method on js's object. loops through the object and pulls out our keys and returns array of keys. for each of these keys
+                    itemCollection[key].id=key; //go through the item collection object by each key and sets the id value to the key value
+                    $scope.items.push(itemCollection[key]); //pushes each itemCollection object to the $scope array
+                });
+            });
+    };   
+
+    getItems();
+
+    $scope.itemDelete = function(inputItemId) {
+        $http
+            .delete(`https://callan-todo.firebaseio.com/items/${inputItemId}.json`)
+            .success(function(){
+                 $scope.items = [];
+                 getItems();
+            });
+    };
 });
